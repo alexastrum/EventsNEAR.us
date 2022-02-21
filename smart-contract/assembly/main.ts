564de1818ace677ddd59baeae44e4226f428963f
Alex Astrum <alexastrum@gmail.com>
2022-02-21 06:31:42 -0500

    Added NEP TODOs

--- smart-contract/assembly/main.ts
+++ smart-contract/assembly/main.ts
@@ -1,8 +1,5 @@
-import { context } from "near-sdk-as";
 import { createEvent_Tier, NFTContract } from "./contract";
-import { PersistentLottery } from "./lottery";
-
-// --- contract code goes below
+import { NFTContractMetadata, Token } from "./nft";
 
 const contract = new NFTContract({
   spec: "nft-1.0.0",
@@ -14,6 +11,62 @@ const contract = new NFTContract({
   reference_hash: "",
 });
 
+// --- NEP-171
+
+export function nft_transfer(
+  receiverId: string,
+  ticketId: string,
+  approval_id: u32,
+  memo: string | null
+): void {
+  contract.nft_transfer(receiverId, ticketId, approval_id, memo);
+}
+
+export function nft_transfer_call(
+  receiver_id: string,
+  token_id: string,
+  approval_id: u32, // not used
+  memo: string | null, // not used
+  msg: string
+): void {
+  contract.nft_transfer_call(receiver_id, token_id, approval_id, memo, msg);
+}
+
+export function nft_token(token_id: string): Token | null {
+  return contract.nft_token(token_id);
+}
+
+export function nft_resolve_transfer(
+  owner_id: string,
+  receiver_id: string,
+  token_id: string,
+  approved_account_ids: any
+): boolean {
+  return contract.nft_resolve_transfer(
+    owner_id,
+    receiver_id,
+    token_id,
+    approved_account_ids
+  );
+}
+
+// --- NEP-177
+
+export function nft_metadata(): NFTContractMetadata {
+  return contract.nft_metadata();
+}
+
+// --- NEP-181
+// TODO
+
+// --- NEP-199
+// TODO
+
+// --- NEP-297
+// TODO
+
+// --- EventsNEAR.us APIs
+
 export function createEvent(
   id: string,
   title: string,
@@ -22,8 +75,6 @@ export function createEvent(
 ): void {
   contract.createEvent(id, title, description, "", 0, "", tickets);
 }
-
-// TODO: Expose NEP-171 APIs
 
 export function transfer(receiverId: string, ticketId: string): void {
   contract.nft_transfer(receiverId, ticketId, 0, "");
